26069b3f7e0e94867cfc836a1e32c7fe9d1169e1
Emerson Pires Pereira <emersonpyres@gmail.com>
2026-07-23 06:21:38 -0300

    fix: CheckSettings fallback and seed tenantId

--- backend/src/database/seeds/20200904070004-create-default-settings.ts
+++ backend/src/database/seeds/20200904070004-create-default-settings.ts
@@ -8,42 +8,49 @@ module.exports = {
         {
           key: "userCreation",
           value: "enabled",
+          tenantId: 1,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           key: "primaryColor",
           value: "#3f51b5",
+          tenantId: 1,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           key: "secondaryColor",
           value: "#f50057",
+          tenantId: 1,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           key: "appName",
           value: "WhaTicket",
+          tenantId: 1,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           key: "appLogoLight",
           value: "",
+          tenantId: 1,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           key: "appLogoDark",
           value: "",
+          tenantId: 1,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           key: "appFavicon",
           value: "",
+          tenantId: 1,
           createdAt: new Date(),
           updatedAt: new Date()
         }
--- backend/src/helpers/CheckSettings.ts
+++ backend/src/helpers/CheckSettings.ts
@@ -2,12 +2,18 @@ import Setting from "../models/Setting";
 import AppError from "../errors/AppError";
 
 const CheckSettings = async (key: string, tenantId = 1): Promise<string> => {
-  const setting = await Setting.findOne({
+  let setting = await Setting.findOne({
     where: { key, tenantId }
   });
 
   if (!setting) {
-    throw new AppError("ERR_NO_SETTING_FOUND", 404);
+    setting = await Setting.findOne({
+      where: { key }
+    });
+  }
+
+  if (!setting) {
+    return "enabled";
   }
 
   return setting.value;
